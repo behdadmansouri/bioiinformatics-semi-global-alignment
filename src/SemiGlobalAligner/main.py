f39595db5c3636e33ec09299985f4db9bb55947d
Behdad Mnasouri <behdadmansouri@aut.ac.ir>
2022-04-04 20:02:44 +0430

    matrix filling passing

--- src/SemiGlobalAligner/main.py
+++ src/SemiGlobalAligner/main.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     x = input("input x\n")
     y = input("input y\n")
     our_matrix = AlignmentMatrix.AlignmentMatrix(x, y)
+    our_matrix.fill_matrix()
     our_matrix.calculate_score()
     our_matrix.calculate_seq()
     our_matrix.print_results()
--- tests/test_AlignmentMatrix.py
+++ tests/test_AlignmentMatrix.py
@@ -1,24 +1,48 @@
 from src.SemiGlobalAligner import AlignmentMatrix
 
-def test_calculate_score():
-    assert False
+def test_pam_reference():
+    our_matrix = AlignmentMatrix.AlignmentMatrix("PAWHEA", "HEAGAWGHE")
+    print(our_matrix.PAM250['A']['C'])
 
-def test_calculate_seq():
-    assert False
+def test_score_PAW():
+    our_matrix = AlignmentMatrix.AlignmentMatrix("PAWHEA", "HEAGAWGHE")
+    our_matrix.fill_matrix()
+    our_matrix.calculate_score()
+    assert our_matrix.score == 0
+
+def test_score_AA():
+    our_matrix = AlignmentMatrix.AlignmentMatrix("AAAAA", "AA")
+    our_matrix.fill_matrix()
+    our_matrix.calculate_score()
+    assert our_matrix.score == 0
+
+def test_calculate_seq0():
+    our_matrix = AlignmentMatrix.AlignmentMatrix("PAWHEA", "HEAGAWGHE")
+    our_matrix.fill_matrix()
+    our_matrix.calculate_score()
+    assert our_matrix.score == 0
+
+def test_calculate_seq1():
+    our_matrix = AlignmentMatrix.AlignmentMatrix("AAAAA", "AA")
+    our_matrix.fill_matrix()
+    our_matrix.calculate_score()
+    assert our_matrix.score == 0
 
 def test_print_results():
-    assert False
+    pass
 
-def test_case_0():
-    our_matrix = AlignmentMatrix.AlignmentMatrix("HEAGAWGHE", "PAWHEA")
-    our_matrix.calculate_score()
-    our_matrix.calculate_seq()
-    assert our_matrix.seq == ""
-    assert our_matrix.score == 0
-    our_matrix.print_results()
-
-def test_case_1():
-    our_matrix = AlignmentMatrix.AlignmentMatrix("AAAAA", "AA")
-    our_matrix.calculate_score()
-    our_matrix.calculate_seq()
-    our_matrix.print_results()
+# def test_case_0():
+#     our_matrix = AlignmentMatrix.AlignmentMatrix("HEAGAWGHE", "PAWHEA")
+#     our_matrix.fill_matrix()
+#     our_matrix.calculate_score()
+#     our_matrix.calculate_seq()
+#     assert our_matrix.seq == ""
+#     assert our_matrix.score == 0
+#     our_matrix.print_results()
+#
+# def test_case_1():
+#     our_matrix = AlignmentMatrix.AlignmentMatrix("AAAAA", "AA")
+#     our_matrix.fill_matrix()
+#     our_matrix.calculate_score()
+#     our_matrix.calculate_seq()
+#     our_matrix.print_results()
